c9048a04ec985f12bb92571099c78d4e69f9d476
riest817 <riest817@gmail.com>
2020-11-17 23:48:34 +0900

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+  このプロジェクトは、Nuxt.jsの練習です。
+  <img src="~/assets/img/huku.png">
+  </div>
+</template>
---
+++ pages/element.vue
@@ -0,0 +1,27 @@
+<template>
+  <div style="padding: 30px;">
+  <el-button @click="visible=true">ダイアログを開く</el-button>
+  <el-dialog :visible.sync="visible" title="確認">
+    <p>処理されました</p>
+  </el-dialog>
+  <el-row>
+  <el-button>Default</el-button>
+  <el-button type="primary">Primary</el-button>
+  <el-button type="success">Success</el-button>
+  <el-button type="info">Info</el-button>
+  <el-button type="warning">Warning</el-button>
+  <el-button type="danger">Danger</el-button>
+</el-row>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      visible: true
+    }
+  }
+}
+</script>
